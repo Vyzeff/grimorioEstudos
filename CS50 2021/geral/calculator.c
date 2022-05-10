337c64312f415251ff91501ef154e40fdd0dd160
Vyzeff <ol.sa.gabriel02@gmail.com>
2022-05-10 14:45:47 -0300

    layout changes

--- CS50 2021/geral/calculator.c
+++
@@ -1,8 +0,0 @@
-// #include <cs50.h> // tem que ser instalado por fora para contar como uma biblioteca valida(acho)
-#include <stdio.h>
-
-int main(void) {
-    long x = get_long("x: ");
-    long y = get_long("y: ");
-    printf("%li/n", x + y);
-}
--- CS50 2021/geral/meow.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-    int i = 0;
-    while (i < 5){
-        printf("meown/n");
-        i++;
-    };
-}
