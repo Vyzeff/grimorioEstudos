523b6daab7db5b3b9217eb0a450f2af6b293ee6a
Vyzeff <ol.sa.gabriel02@gmail.com>
2022-06-06 15:45:33 -0300

    Python Data Structures and Algorithms completion

---
+++ grimorio de estudos/LinkedIn/Python Data Structures and Algorithms/classes/queue_ll.py
@@ -0,0 +1,42 @@
+
+from collections import deque
+
+
+class Queue:
+    def __init__(self):
+        self.items = deque()
+
+    def is_empty(self):
+        return not self.items
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        return self.items.popleft()
+
+    def size(self):
+        return len(self.items)
+
+    def peek(self):
+        return self.items[0]
+
+    def __str__(self):
+        return str(self.items)
+
+
+if __name__ == "__main__":
+    q = Queue()
+
+    print(q)
+    
+    print(q.size)
+    
+    print(q.is_empty)
+    
+    q.enqueue("hehehehehe")
+    q.enqueue("hahahaha")
+    q.enqueue("oooo")
+    print(q.dequeue)
+    print(q.dequeue)
+    print(q.dequeue)
