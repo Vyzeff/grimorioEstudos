6a86207de27d73fe56b8c8c1ec38a233b8fa0197
Vyzeff <ol.sa.gabriel02@gmail.com>
2022-06-08 10:27:00 -0300

    Python OOP completion

--- grimorio de estudos/LinkedIn/Become a Python Dev/thingieClass.py
+++ grimorio de estudos/LinkedIn/Become a Python Dev/thingieClass.py
@@ -2,6 +2,9 @@
 # Basic class definitions
 
 # Create a example class
+from dataclasses import dataclass
+
+
 class Thingie():
     
     # Global class var
@@ -34,14 +37,79 @@ class Thingie():
     def getRelease(self):
         return self._releasedate
     
+    # Comparison 
+    def __eq__(self, value):
+        if not isinstance(value, Thingie):
+            return ValueError("Can't compare object from different class.")
+        return (self._title == value._title
+                and self._type == value._type 
+                and self._releasedate == value._releasedate)
+        
+    # >= function
+    def __ge__(self, value):
+        if not isinstance(value, Thingie):
+            return ValueError("Can't compare object from different class.")
+        return self._releasedate >= value._releasedate
+    
+    # < function
+    def __lt__(self, value):
+        if not isinstance(value, Thingie):
+          return ValueError("Can't compare object from different class.")
+        return self._releasedate < value._releasedate
+    
+    # Callabe object
+    def __call__(self, **kwargs):
+        self._title = kwargs["title"] 
+        self._description = kwargs["description"] if "description" in kwargs else "No description added."
+        self._releasedate = kwargs["release"] if "release" in kwargs else "No release date added"
+        self._type = kwargs["type"] if "type" in kwargs else "GENERIC"
+        
+    # when the th = Thingie() is printed.
     def __str__(self):
         return f"The thingie is called {self._title}"
+    
+    # represents in messages and such
+    def __repr__(self):
+        return f"title={self._title}, releasedate={self._releasedate}, description={self._description}"
         
-# TODO: create instances of the class
+        
+class childThingie(Thingie):
+    def __init__(self):
+        super().__init__
+
+@dataclass
+class gameTime:
+    minutes : int
+    hours : int
+    days : int
+    session : int
+    eventTrigger : str
+        
 th = Thingie(title="Crosscode", description="amazing game", release="2018")
+th2 = Thingie(title="Crosscode", description="amazing game", release="2018")
+th3 = Thingie(title="V Rising", description="cool game", release="2022")
 
-
-# TODO: print the class and property
 print(th.getRelease())
 print(th.getDesc())
 print(th)
+print (th == th2)
+print (th == th3)
+print (th >= th2)
+print(th < th2)
+
+print("-------------")
+
+thingies = [th, th2, th3]
+thingies.sort()
+print(thingies)
+
+print("-------------")
+
+gm = gameTime(1, 1, 1, 1, "laters")
+gm2 = gameTime(1, 1, 1, 1, "laters")
+gm3 = gameTime(2, 3, 4, 5, "lazers")
+
+print(gm.days)
+print(gm2.eventTrigger)
+print(gm3)
+print(gm2 == gm)
