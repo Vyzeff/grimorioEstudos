7a32ff50cb617131452de29661c25158a0072ee7
Vyzeff <ol.sa.gabriel02@gmail.com>
2022-06-15 09:34:24 -0300

    snake movement test

--- grimorio de estudos/LinkedIn/Python Hands  On Practice/snakeGame/turtleGame.py
+++ grimorio de estudos/LinkedIn/Python Hands  On Practice/snakeGame/turtleGame.py
@@ -4,36 +4,54 @@ import turtle
 # Define program constants
 WIDTH = 500
 HEIGHT = 500
-DELAY = 20 # miliseconds for screen updates
+DELAY = 400 # miliseconds for screen updates
 
 def moveSnake(): # manually moves snake
-    mySnake.forward(1)
-    mySnake.right(1)
+    mySnake.clearstamps()
+    
+    newHead = snake[-1].copy()
+    newHead[0] += 20
+    
+    #adds new head to the list
+    snake.append(newHead)
+    
+    # removes old head
+    snake.pop(0)
+    
+    for segment in snake:
+        mySnake.goto(segment[0], segment[1])
+        mySnake.stamp()
+    
+    # refresh screen
     screen.update()
+    
+    # repeats
     screen.ontimer(moveSnake, DELAY)
     
 # Create a window where we will do our drawing.
 screen = turtle.Screen()
 screen.setup(WIDTH, HEIGHT)  # Set the dimensions of the Turtle Graphics window.
-screen.title("Program Title")
+screen.title("Sneaky Snake")
 screen.bgcolor("yellow")
-# screen.tracer(0) # disables auto animation
+screen.tracer(0) # disables auto animation
 
 # Create a turtle to do your bidding
 mySnake = turtle.Turtle()
-mySnake.shape("circle")
-mySnake.color("cyan")
-mySnake.shapesize(50 / 20)
-mySnake.stamp()
+mySnake.shape("square")
+mySnake.color("pink")
 mySnake.penup()
-mySnake.shapesize(10 / 20)
-mySnake.goto(100, 100)
-mySnake.stamp()
+
+# snake coordinates in (x, y)
+snake = [[0, 0], [20, 0], [40, 0], [60, 0]]
+
+# draw snake
+for segment in snake:
+    mySnake.goto(segment[0], segment[1])
+    mySnake.stamp()
 
 # set animation
-#moveSnake()
+moveSnake()
 
-mySnake.forward(100)  # Sample command
 
-# This statement (or an equivalent) is needed at the end of all your turtle programs.
+# finish
 turtle.done()
