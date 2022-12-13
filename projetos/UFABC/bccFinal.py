cc9f4a849be179a7035d2137828b202942b6f90e
Vyzeff <[200~ol.sa.gabriel02@gmail.com~>
2022-12-13 00:11:44 -0300

    bcc final project v2

--- projetos/UFABC/bccFinal.py
+++ projetos/UFABC/bccFinal.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
 from math import ceil
-def csvFindHeavierColumn(csvFile = 'computadores_rotulos.csv', csvColumn = 'performance'):
-    
+from random import randint
+def csvFindRelations(csvFile = 'computadores_rotulos.csv', csvColumn = 'performance', loop=False):
+    print(f"Tabela: {csvFile} \nColuna Alvo: {csvColumn}", end="\n \n")
     #com pandas, ler o arquivo csv
     df = pd.read_csv(csvFile, sep=',', dtype=int);
     
@@ -36,15 +37,19 @@ def csvFindHeavierColumn(csvFile = 'computadores_rotulos.csv', csvColumn = 'perf
     else:
         print(f"A maior correlação de {csvColumn} é {maxCorr}, com uma fraca correlação de {corrLista[maxCorr]}")
 
-    print(f"A menor correlação entre todas com {csvColumn} é aquela de {minCorr}, com uma fraca relação de {corrLista[minCorr]}")
+    print(f"A menor correlação entre todas com {csvColumn} é aquela de {minCorr}, com uma fraca relação de {corrLista[minCorr]}", end="\n \n")
 
+    if loop == False:
+        print("Segue, tambem, uma outra relação completamente aleatória.")
+        randValue = randint(0, len(nomeColunas) -1)
+        csvFindRelations(csvFile, nomeColunas[randValue], loop=True)
     return 0
 
 def main():
     csv = input("Por favor, um csv ou link para csv: ")
     coluna = input("Por fim, digite a coluna desejada para os calculos: ")
     #try:
-    csvFindHeavierColumn(csv, coluna)
+    csvFindRelations(csv, coluna)
     #except:
     #    print("Por favor, verifique se você digitou corretamente os dados e tente novamente.")
 # Main function calling
