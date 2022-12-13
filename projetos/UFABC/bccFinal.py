69f466962f39f71c057bb7deca2a6c8c0f5e448d
Vyzeff <[200~ol.sa.gabriel02@gmail.com~>
2022-12-12 23:13:23 -0300

    bcc final 2nd draft

--- projetos/UFABC/bccFinal.py
+++ projetos/UFABC/bccFinal.py
@@ -1,40 +1,37 @@
-import csv
-import numpy as np
 import pandas as pd
     
-#Porem, tive dificuldade em fazer o csv abrir um link como https://drive.google.com/uc?export=download&id=10zM8jUJZtelgky9Zh5QvPsEI1yxUiy13, então
-#os dados foram extraídos separadamente.
-#Considerando 0 como um diagnostico positivo para a doença e 1 como a presença do sintoma, temos:
-
-def csvFindHeavierColumn(csvFile, csvColumn):
-    #start pandas and read file
+def csvFindHeavierColumn(csvFile = 'computadores_rotulos.csv', csvColumn = 'performance'):
+    
+    #com pandas, ler o arquivo csv
     df = pd.read_csv(csvFile, sep=',', dtype=int);
+    
     #get dataframe columns
-    dataColumns = df.columns
+    nomeColunas = df.columns
+    
     #start dict to store corr values
-    corrList = {}
+    corrLista = {}
+    
+    for coluna in nomeColunas:
+        try:
+            if coluna != csvColumn:
+                corrLista[coluna] = df[csvColumn].corr(df[coluna])
+        except:
+            break
+    
+    print(f"A lista total de correlações do arquivo {csvFile} segue:")
+    for data in corrLista:
+        print(f"{data}: {corrLista[data]:0.3f}")
 
-    colunas = {"ciclo" : 0, "mem_min" : 0, "mem_max" : 0, "cache" : 0, "min_canais" : 0, "max_canais" : 0}
-    dictKeys = list(colunas.keys())
 
-    for x in range(len(dictKeys)):
-        colunas[dictKeys[x]] = df[csvColumn].corr(df[dictKeys[x]])
-
-    print(colunas)
-    maxCorr = max(colunas.values())
-    print(f"A coluna com a maior correlação com {csvColumn} é {maxCorr}")
     return 0
 
 def main():
-    csvFindHeavierColumn('computadores_rotulos.csv', 'performance')
-
+    csv = input("Por favor, um csv ou link para csv: ")
+    coluna = input("Por fim, digite a coluna desejada para os calculos: ")
+    try:
+        csvFindHeavierColumn(csv, coluna)
+    except:
+        print("Por favor, verifique se você digitou corretamente os dados e tente novamente.")
 # Main function calling
 if __name__ == "__main__":
     main()
-    
-
-
-    #for column in dictKeys:
-    #    if column != csvColumn:
-    #        print(column)
-    #       colunas["{column}"] = df[csvColumn].corr(df[column])
