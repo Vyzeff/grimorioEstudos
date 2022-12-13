f0745e7dc364d807b4879034bf8b86ed9a80c974
Vyzeff <[200~ol.sa.gabriel02@gmail.com~>
2022-12-13 10:06:21 -0300

    bcc final project vFinal: added a few comments and sample table links

--- projetos/UFABC/bccFinal.py
+++ projetos/UFABC/bccFinal.py
@@ -2,6 +2,12 @@ import pandas as pd
 import numpy as np
 from math import ceil
 from random import randint
+
+#Link para a tabela de computadores: https://drive.google.com/uc?export=download&id=1D605NkuC-c5uh5L0iErZ00bFB6wGiz-F
+#Link para tabela de imoveis: https://drive.google.com/uc?export=download&id=1CN8AMb-XoRn5EvpzwKSJHDZiU6mBSHB6
+#Atualmente, a tabela de imoveis não funciona por conta dos números serem separador por virgulas, o que quebra o pandas
+#Por enquanto, em teoria é possivel usar qualquer tabela que tenha somente números separados por . em suas colunas.
+
 def csvFindRelations(csvFile = 'computadores_rotulos.csv', csvColumn = 'performance', loop=False):
     print(f"Tabela: {csvFile} \nColuna Alvo: {csvColumn}", end="\n \n")
     #com pandas, ler o arquivo csv
@@ -23,7 +29,7 @@ def csvFindRelations(csvFile = 'computadores_rotulos.csv', csvColumn = 'performa
             break
     
     #printa a lista de correlações com 3 numeros apos o 0
-    print(f"A lista total de correlações do arquivo {csvFile} segue:")
+    print(f"A lista total de correlações do arquivo {csvFile} segue com a coluna {csvColumn}:")
     for data in corrLista:
         print(f"{data}: {corrLista[data]:0.3f}")
         
@@ -37,14 +43,14 @@ def csvFindRelations(csvFile = 'computadores_rotulos.csv', csvColumn = 'performa
     
     #arredonda o maximo e verifica o grau da correlação
     if ceil(corrLista[maxCorr]) >= 0.7:
-        print(f"A maior correlação de {csvColumn} é {maxCorr}, com uma forte correlação de {corrLista[maxCorr]}")
+        print(f"A maior correlação de {csvColumn} é {maxCorr}, com uma forte correlação de {corrLista[maxCorr]:0.3f}")
     elif ceil(corrLista[maxCorr]) >= 0.4: 
-        print(f"A maior correlação de {csvColumn} é {maxCorr}, com uma media correlação de {corrLista[maxCorr]}")
+        print(f"A maior correlação de {csvColumn} é {maxCorr}, com uma media correlação de {corrLista[maxCorr]:0.3f}")
     else:
-        print(f"A maior correlação de {csvColumn} é {maxCorr}, com uma fraca correlação de {corrLista[maxCorr]}")
+        print(f"A maior correlação de {csvColumn} é {maxCorr}, com uma fraca correlação de {corrLista[maxCorr]:0.3f}")
 
     #printa a menor correlação achada
-    print(f"A menor correlação entre todas com {csvColumn} é aquela de {minCorr}, com uma fraca relação de {corrLista[minCorr]}", end="\n \n")
+    print(f"A menor correlação entre todas com {csvColumn} é aquela de {minCorr}, com uma fraca relação de {corrLista[minCorr]:0.3f}", end="\n \n")
 
     #se a variavel loop for falsa, executa a função
     if loop == False:
@@ -57,7 +63,7 @@ def main():
     csv = input("Por favor, um csv ou link para csv: ")
     coluna = input("Por fim, digite a coluna desejada para os calculos: ")
     try:
-        csvFindRelations(csv, coluna)
+      csvFindRelations(csv, coluna, False)
     except:
         print("Por favor, verifique se você digitou corretamente os dados e tente novamente.")
 # Main function calling
