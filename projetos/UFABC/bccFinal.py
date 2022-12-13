5a8bfc0d8b4fcb189b43848945f47dc68004741b
Vyzeff <[200~ol.sa.gabriel02@gmail.com~>
2022-12-12 23:48:31 -0300

    bcc final project v1

--- projetos/UFABC/bccFinal.py
+++ projetos/UFABC/bccFinal.py
@@ -1,5 +1,6 @@
 import pandas as pd
-    
+import numpy as np
+from math import ceil
 def csvFindHeavierColumn(csvFile = 'computadores_rotulos.csv', csvColumn = 'performance'):
     
     #com pandas, ler o arquivo csv
@@ -21,17 +22,31 @@ def csvFindHeavierColumn(csvFile = 'computadores_rotulos.csv', csvColumn = 'perf
     print(f"A lista total de correlações do arquivo {csvFile} segue:")
     for data in corrLista:
         print(f"{data}: {corrLista[data]:0.3f}")
+        
+    primeiraMedia = np.median(list(corrLista.values()))
+    print(f"A media de todas as correlações com {csvColumn} é de {primeiraMedia:0.3f}.", end="\n \n")
+    
+    maxCorr =  max(corrLista, key=corrLista.get)
+    minCorr = min(corrLista, key=corrLista.get)
+    
+    if ceil(corrLista[maxCorr]) >= 0.7:
+        print(f"A maior correlação de {csvColumn} é {maxCorr}, com uma forte correlação de {corrLista[maxCorr]}")
+    elif ceil(corrLista[maxCorr]) >= 0.4: 
+        print(f"A maior correlação de {csvColumn} é {maxCorr}, com uma media correlação de {corrLista[maxCorr]}")
+    else:
+        print(f"A maior correlação de {csvColumn} é {maxCorr}, com uma fraca correlação de {corrLista[maxCorr]}")
 
+    print(f"A menor correlação entre todas com {csvColumn} é aquela de {minCorr}, com uma fraca relação de {corrLista[minCorr]}")
 
     return 0
 
 def main():
     csv = input("Por favor, um csv ou link para csv: ")
     coluna = input("Por fim, digite a coluna desejada para os calculos: ")
-    try:
-        csvFindHeavierColumn(csv, coluna)
-    except:
-        print("Por favor, verifique se você digitou corretamente os dados e tente novamente.")
+    #try:
+    csvFindHeavierColumn(csv, coluna)
+    #except:
+    #    print("Por favor, verifique se você digitou corretamente os dados e tente novamente.")
 # Main function calling
 if __name__ == "__main__":
     main()
