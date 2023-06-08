e0989319d07d4c3f1a3395f89f8bd8c03712ae8d
alllga <58573757+alllga@users.noreply.github.com>
2023-06-07 21:33:47 -0300

    Delete UFABC Directory to make changes

--- UFABC/PI2023/Exercicios/ex01.py
+++
@@ -1,5 +0,0 @@
-# EXERCICIO 01
-# Hello World
-# Testes automatizados.
-
-print("Alo, mundo!")
--- UFABC/PI2023/Exercicios/ex10.py
+++
@@ -1,3 +0,0 @@
-#   EXERCICIO 10
-# Calcular o numero de caixar baseado na capacidade maxima de um caminhão
-# Testes automatizados.
